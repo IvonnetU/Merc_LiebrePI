0e28f73dcd5f5b46ae07ec49ec6af19dfd1a6daf
IvonnetU <ivonnetorresu@gmail.com>
2021-10-27 08:03:13 -0500

    subiendo repo

---
+++ app.js
@@ -0,0 +1,17 @@
+
+const express = require('express');
+
+const app = express();
+const path = require('path');
+
+const port = process.env.PORT || 3000;
+const publicPath = path.resolve(__dirname, './public');
+app.use(express.static(publicPath));
+
+app.listen(port,() => {
+    console.log("start server");
+});
+
+app.get('/', (req, res) => {
+    res.sendFile(path.resolve(__dirname, './views/home.html'));
+});
